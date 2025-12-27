cc8beb9971189f925e67ea5b800bbd6fdb9e8c57
Maksim <zokermm@gmail.com>
2025-12-27 20:58:29 +0100

    Add language switcher for filament

--- config/multisite.php
+++ config/multisite.php
@@ -6,4 +6,33 @@ return [
      * If null, canonical link will not be generated
      */
     'canonical_site_id' => null,
+
+    /**
+     * Available locales for Filament interface
+     */
+    'locales' => [
+        'en' => [
+            'name' => 'English',
+            'icon' => 'heroicon-o-language',
+        ],
+        // Uncomment to add more languages
+/*
+        'ru' => [
+            'name' => 'Русский',
+            'icon' => 'heroicon-o-language',
+        ],
+        'sl' => [
+            'name' => 'Slovenščina',
+            'icon' => 'heroicon-o-language',
+        ],
+        'de' => [
+            'name' => 'Deutsch',
+            'icon' => 'heroicon-o-language',
+        ],
+        'es' => [
+            'name' => 'Español',
+            'icon' => 'heroicon-o-language',
+        ],
+*/
+    ],
 ];
--- src/Multisite.php
+++ src/Multisite.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Zoker\FilamentMultisite;
 
+use Filament\Actions\Action;
 use Filament\Contracts\Plugin;
+use Filament\Forms\Components\Select;
 use Filament\Panel;
 use LaraZeus\SpatieTranslatable\SpatieTranslatablePlugin;
 use Zoker\FilamentMultisite\Models\Site;
@@ -28,5 +30,32 @@ final class Multisite implements Plugin
         $panel->plugin(SpatieTranslatablePlugin::make()->defaultLocales(Site::getUsingLocales())->persist());
     }
 
-    public function boot(Panel $panel): void {}
+    public function boot(Panel $panel): void
+    {
+        $locales = config('multisite.locales', []);
+
+        if (count($locales) > 1) {
+            $panel->userMenuItems([
+                Action::make('language_picker')
+                    ->label('Change Language')
+                    ->schema([
+                        Select::make('language')
+                            ->options(fn() => array_map(fn ($config) => $config['name'], $locales))
+                            ->default(fn() => app()->getLocale())
+                            ->required()
+                    ])
+                    ->action(function (array $data) {
+                        $availableLocales = array_keys(config('multisite.locales', []));
+
+                        if (! in_array($data['language'], $availableLocales)) {
+                            return;
+                        }
+
+                        cookie()->queue('filament_locale', $data['language'], 60 * 60 * 24 * 365 * 10);
+
+                        redirect(url()->previous());
+                    })
+            ]);
+        }
+    }
 }
