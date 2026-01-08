f4284ecbc7bda831e21239815baf6244c32dd97e
Maksim <zokermm@gmail.com>
2026-01-08 18:24:32 +0100

    Fix save 2 translations

--- src/Traits/Translatable/Resources/Pages/TranslatableCreateRecord.php
+++ src/Traits/Translatable/Resources/Pages/TranslatableCreateRecord.php
@@ -3,9 +3,17 @@
 namespace Zoker\FilamentMultisite\Traits\Translatable\Resources\Pages;
 
 use LaraZeus\SpatieTranslatable\Resources\Pages\CreateRecord\Concerns\Translatable;
+use Zoker\FilamentMultisite\Facades\FilamentSiteManager;
 use Zoker\FilamentMultisite\Traits\Translatable\HasActiveSiteSwitcher;
 
 trait TranslatableCreateRecord
 {
     use HasActiveSiteSwitcher, Translatable;
+
+    public function initializeTranslatableCreateRecord(): void
+    {
+        if (request()->getMethod() == 'POST') {
+            app()->setLocale(FilamentSiteManager::getCurrentSite()->locale);
+        }
+    }
 }
